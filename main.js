6c05dceb6691c757d390695474880d55dc65399e
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-02-25 20:36:04 -0600

    producto

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Fecha from "./fecha.js";
 import Tiempo from "./tiempo.js";
 import Direccion from "./direccion.js";
 import Precio from "./precio.js";
+import Producto from "./producto.js";
 
 class Main {
   constructor() {
@@ -9,6 +10,7 @@ class Main {
     this.tiempo = new Tiempo(7, 30, "pm");
     this.direccion = new Direccion("Av. Insurgentes",321,2,"Miguel Hidalgo",28100,"Tecoman","Armeria");
     this.precio = new Precio(1700.00)
+    this.producto = new Producto("Bolillo Seco", this.precio)
 
   }
   probarFecha() {
@@ -30,6 +32,9 @@ class Main {
   probarPrecio() {
       console.log(`${this.precio.getPrecio()}`)
   }
+  probarProducto() {
+      console.log(`${this.producto.getDescripcion()}`)
+  }
 }
 let app = new Main
-app.probarPrecio()
+app.probarProducto()
