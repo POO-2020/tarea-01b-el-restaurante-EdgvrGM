fd6a1f19c5af3c9339b20ba1ddefa9fbc82bd466
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-02-25 19:51:53 -0600

    tiempo

---
+++ tiempo.js
@@ -0,0 +1,23 @@
+export default class Tiempo {
+    /**
+     *
+     * @param {number} hora
+     * @param {number} minutos
+     * @param {string} periodo
+     */
+    constructor(hora, minutos, periodo) {
+      this.hora = hora;
+      this.minutos = minutos;
+      this.periodo = periodo;
+    }
+    getFormato12() {
+      return `${this.hora}:${this.minutos} ${this.periodo}`;
+    }
+    getFormato24() {
+      if (this.periodo == "pm") {
+        return `${this.hora + 12}:${this.minutos}`;
+      } else if (this.periodo == "am") {
+        return `${this.hora}:${this.minutos}`;
+      }
+    }
+  }
